6466a9f74dc11b789f25466455b48bc6ee0e4426
John Oilar <johnoilar@gmail.com>
2014-12-20 19:54:18 -0500

    Updated insert queries and added CardGroups table.

--- db/basic.sql
+++ db/basic.sql
@@ -14,30 +14,59 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+PRAGMA foreign_keys = ON;
+
 DROP TABLE IF EXISTS "Cards";
 CREATE TABLE "Cards" ("id" INTEGER PRIMARY KEY  NOT NULL , "name" VARCHAR, "value" INTEGER, "text" VARCHAR);
 
-INSERT INTO "Cards" VALUES(1,'Pottery',45,'During FAMINE, reduce loss by 4 unit points per GRAIN trade card held.');
-INSERT INTO "Cards" VALUES(2,'Cloth Making',45,'Ships allowed an extra move.');
-INSERT INTO "Cards" VALUES(3,'Agriculture',110,'Increases population limit by 1 in any area occupied alone.');
-INSERT INTO "Cards" VALUES(4,'Metalworking',80,'In battle, remove tokens after players without METALWORKING have removed theirs.');
-INSERT INTO "Cards" VALUES(5,'Drama & Poetry',60,'');
-INSERT INTO "Cards" VALUES(6,'Music',60,'');
-INSERT INTO "Cards" VALUES(7,'Astronomy',80,'Permits voyages across open seas.');
-INSERT INTO "Cards" VALUES(8,'Coinage',110,'Each round, may fix taxation at 1, 2, or 3 tokens per city. Same rate must be applied towards all cities.');
-INSERT INTO "Cards" VALUES(9,'Medicine',140,'During EPIDEMIC, lose only 11 unit points. Lose at most 5 unit points due to EPIDEMIC order.');
-INSERT INTO "Cards" VALUES(10,'Engineering',140,'Opponents must have at least 8 tokens to attack. Lose 7 unit points at most due to FLOOD.');
-INSERT INTO "Cards" VALUES(11,'Literacy',110,'');
-INSERT INTO "Cards" VALUES(12,'Architecture',80,'');
-INSERT INTO "Cards" VALUES(13,'Mysticism',30,'');
-INSERT INTO "Cards" VALUES(14,'Democracy',200,'During CIVIL WAR, 45 units form the first faction: 15 chosen by player; 30 chosen by nominee. During CIVIL DISORDER, only cities in excess of 6 are reduced.');
-INSERT INTO "Cards" VALUES(15,'Law',170,'During ICONOCLASM & HERESY, lose only 3 cities. During HERESY orders, lose only 1 city. During CIVIL DISORDER, only cities in excess of 5 are reduced.');
-INSERT INTO "Cards" VALUES(16,'Philosophy',240,'During ICONOCLASM & HERESY, lose only 2 cities. Immune to HERESY orders. During CIVIL WAR, only 15 units form first faction. Units chosen by player with most tokens in stock.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Pottery',45,'During FAMINE, reduce loss by 4 unit points per GRAIN trade card held.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Cloth Making',45,'Ships allowed an extra move.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Agriculture',110,'Increases population limit by 1 in any area occupied alone.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Metalworking',80,'In battle, remove tokens after players without METALWORKING have removed theirs.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Drama & Poetry',60,'');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Music',60,'');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Astronomy',80,'Permits voyages across open seas.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Coinage',110,'Each round, may fix taxation at 1, 2, or 3 tokens per city. Same rate must be applied towards all cities.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Medicine',140,'During EPIDEMIC, lose only 11 unit points. Lose at most 5 unit points due to EPIDEMIC order.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Engineering',140,'Opponents must have at least 8 tokens to attack. Lose 7 unit points at most due to FLOOD.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Literacy',110,'');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Architecture',80,'');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Mysticism',30,'');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Democracy',200,'During CIVIL WAR, 45 units form the first faction: 15 chosen by player; 30 chosen by nominee. During CIVIL DISORDER, only cities in excess of 6 are reduced.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Law',170,'During ICONOCLASM & HERESY, lose only 3 cities. During HERESY orders, lose only 1 city. During CIVIL DISORDER, only cities in excess of 5 are reduced.');
+INSERT INTO "Cards" ("name", "value", "text") VALUES ('Philosophy',240,'During ICONOCLASM & HERESY, lose only 2 cities. Immune to HERESY orders. During CIVIL WAR, only 15 units form first faction. Units chosen by player with most tokens in stock.');
 
 DROP TABLE IF EXISTS "Groups";
 CREATE TABLE "Groups" ("id" INTEGER PRIMARY KEY  NOT NULL , "name" VARCHAR, "credit" INTEGER);
 
-INSERT INTO "Groups" VALUES(1,'Crafts',10);
-INSERT INTO "Groups" VALUES(2,'Arts',5);
-INSERT INTO "Groups" VALUES(3,'Science',20);
-INSERT INTO "Groups" VALUES(4,'Civics',0);
+INSERT INTO "Groups" ("name", "credit") VALUES ('Crafts',10);
+INSERT INTO "Groups" ("name", "credit") VALUES ('Arts',5);
+INSERT INTO "Groups" ("name", "credit") VALUES ('Science',20);
+INSERT INTO "Groups" ("name", "credit") VALUES ('Civics',0);
+
+DROP TABLE IF EXISTS "CardGroups";
+CREATE TABLE "CardGroups" ("cardId" REFERENCES Cards, "groupId" REFERENCES Groups);
+
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Pottery'), (SELECT "id" FROM "Groups" WHERE "name" = 'Crafts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Cloth Making'), (SELECT "id" FROM "Groups" WHERE "name" = 'Crafts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Agriculture'), (SELECT "id" FROM "Groups" WHERE "name" = 'Crafts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Metalworking'), (SELECT "id" FROM "Groups" WHERE "name" = 'Crafts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Drama & Poetry'), (SELECT "id" FROM "Groups" WHERE "name" = 'Arts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Music'), (SELECT "id" FROM "Groups" WHERE "name" = 'Arts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Astronomy'), (SELECT "id" FROM "Groups" WHERE "name" = 'Science'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Coinage'), (SELECT "id" FROM "Groups" WHERE "name" = 'Science'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Medicine'), (SELECT "id" FROM "Groups" WHERE "name" = 'Science'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Engineering'), (SELECT "id" FROM "Groups" WHERE "name" = 'Crafts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Engineering'), (SELECT "id" FROM "Groups" WHERE "name" = 'Science'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Literacy'), (SELECT "id" FROM "Groups" WHERE "name" = 'Arts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Literacy'), (SELECT "id" FROM "Groups" WHERE "name" = 'Civics'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Architecture'), (SELECT "id" FROM "Groups" WHERE "name" = 'Arts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Architecture'), (SELECT "id" FROM "Groups" WHERE "name" = 'Civics'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Mysticism'), (SELECT "id" FROM "Groups" WHERE "name" = 'Arts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Mysticism'), (SELECT "id" FROM "Groups" WHERE "name" = 'Science'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Democracy'), (SELECT "id" FROM "Groups" WHERE "name" = 'Crafts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Democracy'), (SELECT "id" FROM "Groups" WHERE "name" = 'Civics'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Law'), (SELECT "id" FROM "Groups" WHERE "name" = 'Civics'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Law'), (SELECT "id" FROM "Groups" WHERE "name" = 'Arts'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Philosophy'), (SELECT "id" FROM "Groups" WHERE "name" = 'Civics'));
+INSERT INTO "CardGroups" VALUES((SELECT "id" FROM "Cards" WHERE "name" = 'Philosophy'), (SELECT "id" FROM "Groups" WHERE "name" = 'Science'));
